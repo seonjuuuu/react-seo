013a084fd2130ebf4e38187db91cc3acffcf283d
MoonSsunny <moonsun116@naver.com>
2024-04-03 12:06:03 +0900

    메인 content 바꾸기

--- src/App.js
+++ src/App.js
@@ -102,17 +102,7 @@ function App() {
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>SEO 최적화 프로젝트</p>
       </header>
     </div>
   );
